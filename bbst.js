9bff50bd21e48cc2679ae7bfc4222f4583a5ad36
Xian-T580 <80217731+V-Le@users.noreply.github.com>
2023-11-11 17:36:19 -0600

    adding fuctionality to buildTree() for left and right node

--- bbst.js
+++ bbst.js
@@ -40,26 +40,21 @@ function buildTree(array, start, end) {
 }
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
-    if (node === null) {
-      return;
-    }
-    if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-    }
+    if (node === null) return;
+    if (node.right !== null) prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+
     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-    if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-    }
+    if (node.left !== null) prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
   };
 
 let myArray = sortUndupe([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 console.log(myArray);
 // console.log(buildTree(myArray, 0, myArray.length))
 // console.log(prettyPrint(buildTree(myArray, 0, myArray.length)))
-prettyPrint(buildTree(myArray, 0, myArray.length));
+prettyPrint(buildTree(myArray, 0, myArray.length-1));
 
 
 //                    8
 //               4          67
-//             1   5     9     6345
-//              3   7     23  324   
+//             1   5     9     324
+//              3   7     23     6345   
