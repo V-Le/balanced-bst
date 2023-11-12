a89ae359ee18c914f17034ee9fe54edbc9b06825
Xian-T580 <80217731+V-Le@users.noreply.github.com>
2023-11-12 14:04:53 -0600

    built out Tree inner functions buildTree() sortRemoveDupes()

--- bbst.js
+++ bbst.js
@@ -1,11 +1,3 @@
-// 1. Build a Node class/factory. It should have an attribute for the data it stores as well as its left and right children.
-
-// 2. Build a Tree class/factory which accepts an array when initialized. The Tree class should have a root attribute, which uses the return value of buildTree which you’ll write next.
-
-// 3 Write a buildTree function that takes an array of data (e.g., [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]) and turns it into a balanced binary tree full of Node objects appropriately placed (don’t forget to sort and remove duplicates!). The buildTree function should return the level-0 root node.
-
-// Tip: If you would like to visualize your binary search tree, here is a prettyPrint() function that will console.log your tree in a structured format. This function will expect to receive the root of your tree as the value for the node parameter.
-
 class Node {
     constructor (data) {
         this.data = data;
@@ -17,44 +9,48 @@ class Node {
 class Tree {
     constructor(array) {
         this.array = array;
-        this.root = buildTree();
+        let sortedArray = this.#sortRemoveDupes(array);
+        this.root = this.#buildTree(sortedArray, 0, sortedArray.length-1);
+    }
+    
+    #sortRemoveDupes(array) {
+        let sortedArray = array.sort((a,b) => a-b); //sorts array
+        return [...new Set (sortedArray)]; //removes dupes and returns
+    }
+    
+    #buildTree(sortedArray, start, end) {
+        if (start > end) return null;
+        
+        let mid = Math.floor((start + end) / 2);
+        let node = new Node(sortedArray[mid]);
+        
+        node.left = this.#buildTree(sortedArray, start, mid-1);
+        node.right = this.#buildTree(sortedArray, mid+1, end);
+        
+        return node;
     }
 }
 
-function sortUndupe(array) {
-    let sortedArray = array.sort((a,b) => a-b); //sorts array
-    return [...new Set (sortedArray)]; //removes dupes and returns
-}
-
-function buildTree(array, start, end) {
-    if (start > end) return null;
-
-    let mid = parseInt((start + end) / 2);
-    let node = new Node(array[mid]);
-    
-    node.left = buildTree(array, start, mid-1);
-    node.right = buildTree(array, mid+1, end);
-    
-    return node;
-
-}
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
     if (node === null) return;
     if (node.right !== null) prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-
     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
     if (node.left !== null) prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-  };
+};
 
-let myArray = sortUndupe([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-console.log(myArray);
-// console.log(buildTree(myArray, 0, myArray.length))
-// console.log(prettyPrint(buildTree(myArray, 0, myArray.length)))
-prettyPrint(buildTree(myArray, 0, myArray.length-1));
+let myArray = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+prettyPrint(myArray.root);
 
 
-//                    8
-//               4          67
-//             1   5     9     324
-//              3   7     23     6345   
+// │           ┌── 6345
+// │       ┌── 324
+// │   ┌── 67
+// │   │   │   ┌── 23
+// │   │   └── 9
+// └── 8
+//     │       ┌── 7
+//     │   ┌── 5
+//     └── 4
+//         │   ┌── 3
+//         └── 1
